a4983defa503df38cdc4163102d454b4912e6f1b
Fatima Camero <fcamero12@gmail.com>
2022-03-24 07:00:47 -0700

    create TimeCard header

--- src/components/TimeCard/TimeCard.js
+++ src/components/TimeCard/TimeCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './TimeCard.module.css'
 
 const TimeCard = () => {
+  const headerBackgroundColor = getComputedStyle(document.body).getPropertyValue('--lightRed1')
+  // console.log('color: ', headerBackgroundColor)
   return (
-    <div>
-      <div>header with icon</div>
+    <div className={styles.card}>
+      <div 
+        className={styles.header}
+        style={{backgroundColor: headerBackgroundColor}}
+      >
+        <img src={require('../../images/icon-work.svg')} />
+      </div>
       <div>
         <div>
           <div>Work</div>
