e768783c12aa8bc2f81198dbdde6a3c4f2308c11
Fatima Camero <fcamero12@gmail.com>
2022-03-29 06:34:43 -0700

    update TimeCard style

--- src/components/TimeCard/TimeCard.js
+++ src/components/TimeCard/TimeCard.js
@@ -7,20 +7,23 @@ const TimeCard = () => {
   return (
     <div className={styles.card}>
       <div 
-        className={styles.header}
+        className={styles.background}
         style={{backgroundColor: headerBackgroundColor}}
       >
-        <img src={require('../../images/icon-work.svg')} />
-      </div>
-      <div className={styles.main}>
-        <div className={styles.flexGrid}>
-          <div className={styles.title}>Work</div>
-          <div className={styles.ellipsis}>
-            <img src={require('../../images/icon-ellipsis.svg')} />
-          </div>
-          <div className={styles.hours}>32hrs</div>
-          <div className={styles.lastHours}>Last Week - 36hrs</div>
+        <div className={styles.iconWrapper}>
+          <img src={require('../../images/icon-work.svg')} />
         </div>
+        <div className={styles.main}>
+          <div className={styles.flexGrid}>
+            <div className={styles.title}>Work</div>
+            <div className={styles.ellipsis}>
+              <img src={require('../../images/icon-ellipsis.svg')} />
+            </div>
+            <div className={styles.hours}>32hrs</div>
+            <div className={styles.lastHours}>Last Week - 36hrs</div>
+          </div>
+        </div>
+      
       </div>
     </div>
   )
