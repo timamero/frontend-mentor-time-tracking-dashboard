5257731fa985827c32cf11c3b79ecaa245c1bd0d
Fatima Camero <fcamero12@gmail.com>
2022-04-08 06:34:06 -0700

    add hover and active style

--- src/components/TimeCard/TimeCard.js
+++ src/components/TimeCard/TimeCard.js
@@ -15,7 +15,13 @@ const TimeCard = () => {
           <div className={styles.flexGrid}>
             <div className={styles.title}>Work</div>
             <div className={styles.ellipsis}>
-              <img src={require('../../images/icon-ellipsis.svg')} />
+              <svg
+                width="21"
+                height="5" 
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill-rule="evenodd"/>
+              </svg>
             </div>
             <div className={styles.hours}>32hrs</div>
             <div className={styles.lastHours}>Last Week - 36hrs</div>
