5a3cd562487defab2769ba9b88be2ed5edc180d2
Fatima Camero <fcamero12@gmail.com>
2022-04-19 06:16:55 -0700

    fetch time data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
-import axios from 'axios'
+// import axios from 'axios'
 
 const App = () => {
-  axios({method: 'get', url: 'http://localhost:3001/daily'})
-    .then(response => console.log(response))
+  // axios({method: 'get', url: 'http://localhost:3001/daily'})
+  //   .then(response => console.log(response))
   return (
     <div className="container">
       <Dashboard />
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import styles from './Dashboard.module.css'
 import UserCard from '../UserCard'
 import TimeCard from '../TimeCard'
 
 const Dashboard = () => {
+  const [period, setPeriod] = useState('daily')
+  const [timeData, setTimeData] = useState(null)
+
+  useEffect(() => {
+    console.log('getting data from period: ', period)
+    axios({method: 'get', url: `http://localhost:3001/${period}`})
+    .then(response => setTimeData(response.data))
+  }, [period])
+
+  if (timeData) {
+    console.log('time data', timeData)
+  }
+
+  const changePeriod = (newPeriod) => {
+    setPeriod(newPeriod)
+  }
+
   return (
     <div className={styles.dashboard}>
       <UserCard />
